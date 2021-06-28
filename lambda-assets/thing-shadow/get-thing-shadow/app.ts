84cf71003dc926a31dddb85647e4a603722774e1
Jason Liao <jasonliao@softchef.com>
2021-06-28 16:59:47 +0800

    Delete yarn.lock

--- lambda-assets/thing-shadow/get-thing-shadow/app.ts
+++ lambda-assets/thing-shadow/get-thing-shadow/app.ts
@@ -1,6 +1,7 @@
 import { IoTDataPlaneClient, GetThingShadowCommand } from "@aws-sdk/client-iot-data-plane";
 import { Request, Response } from '../../utils';
 
+
 export async function handler(event: { [key: string]: any }) {
   const request = new Request(event);
   const response = new Response();
