69f9c933bfdb0a16ea44d6330a8a888aa9a23ea9
Jason Liao <jasonliao@softchef.com>
2021-06-29 18:29:18 +0800

    Add cancel-job

---
+++ lambda-assets/jobs/cancel-job/app.ts
@@ -0,0 +1,20 @@
+import { CancelJobCommand, IoTClient } from '@aws-sdk/client-iot';
+import { Request, Response } from '../../utils';
+
+export async function handler(event: { [key: string]: any }) {
+  const request = new Request(event);
+  const response = new Response();
+  try {
+    const iotClient = new IoTClient({});
+    await iotClient.send(
+      new CancelJobCommand({
+        jobId: request.parameter('jobId'),
+      }),
+    );
+    return response.json({
+      canceled: true,
+    });
+  } catch (error) {
+    return response.error(error);
+  }
+}
