7dc0ad9c85d4bd15a554a17a17ba680f765a6844
Kerwin <kerwinjhong@softchef.com>
2021-07-19 16:25:59 +0800

    Modify

--- lambda-assets/files/update-file/app.ts
+++ lambda-assets/files/update-file/app.ts
@@ -1,6 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, QueryCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand, UpdateCommand, BatchWriteCommand } from '@aws-sdk/lib-dynamodb';
 import { Request, Response } from '@softchef/lambda-events';
+import * as lodash from 'lodash';
 
 export async function handler(event: { [key: string]: any }) {
   const request = new Request(event);
@@ -8,10 +9,14 @@ export async function handler(event: { [key: string]: any }) {
   try {
     const validated = request.validate(joi => {
       return {
-        files: joi.array().items(
+        files: joi.array().min(1).items(
           joi.object().keys({
             fileId: joi.string().required(),
             locale: joi.string().required(),
+            checksumType: joi.string().allow('md5', 'crc32', 'sha1'),
+            checksum: joi.string().when('checksumType', { is: 'md5', then: joi.string().length(32).required() })
+              .concat(joi.string().when('checksumType', { is: 'crc32', then: joi.string().length(8).required() }))
+              .concat(joi.string().when('checksumType', { is: 'sha1', then: joi.string().length(40).required() })),
             summary: joi.string().allow(null, ''),
             description: joi.string().allow(null, ''),
           }),
@@ -24,6 +29,13 @@ export async function handler(event: { [key: string]: any }) {
     const ddbDocClient = DynamoDBDocumentClient.from(
       new DynamoDBClient({}),
     );
+    const files = request.input('files');
+    const firstFile = lodash.first(files);
+    files.map(currentFile => {
+      if (currentFile.checksum !== firstFile.checksum) {
+        return response.error('File\'s checksum is not same.', 400);
+      };
+    });
     const { Items: existsFiles } = await ddbDocClient.send(
       new QueryCommand({
         TableName: process.env.FILE_TABLE_NAME,
@@ -34,7 +46,7 @@ export async function handler(event: { [key: string]: any }) {
           '#version': 'version',
         },
         ExpressionAttributeValues: {
-          ':checksum': request.parameter('checksum'),
+          ':checksum': firstFile.checksum,
           ':version': request.parameter('version'),
         },
       }),
@@ -43,18 +55,16 @@ export async function handler(event: { [key: string]: any }) {
       return response.error('File not found.', 404);
     };
     await ddbDocClient.send(
-      new UpdateCommand({
-        TableName: process.env.FILE_TABLE_NAME,
-        Key: {
-          fileId: request.parameter('fileId'),
-          version: request.parameter('version'),
-        },
-        UpdateExpression: 'set #description = :description',
-        ExpressionAttributeNames: {
-          '#description': 'description',
-        },
-        ExpressionAttributeValues: {
-          ':description': request.input('description', ''),
+      new BatchWriteCommand({
+        RequestItems: {
+          [`${process.env.FILE_TABLE_NAME}`]: existsFiles.map(existsFile => {
+            const currentLocaleFile = files.find(v => v.locale === existsFile.locale);
+            return {
+              PutRequest: {
+                Item: Object.assign({}, existsFile, currentLocaleFile),
+              },
+            };
+          });
         },
       }),
     );
