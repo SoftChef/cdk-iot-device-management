711fb3fe711fbfd376aedfc9c7db4e2a60701ac5
MinChe Tsai <tirme0812@gmail.com>
2021-08-11 11:55:20 +0800

    Remove .history

--- .history/src/index_20210628225607.ts
+++
@@ -1,5 +0,0 @@
-export * from './thing-type-api';
-export * from './thing-api';
-export * from './thing-group-api';
-export * from './file-api';
-export * from './job-api';
--- .history/src/index_20210729100722.ts
+++
@@ -1,5 +0,0 @@
-export * from './file-api';
-export * from './job-api';
-export * from './thing-api';
-export * from './thing-group-api';
-export * from './thing-type-api';
